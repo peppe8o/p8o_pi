b361a1344c5292ea340cdc47581024151a24b623
peppe8o <68339686+peppe8o@users.noreply.github.com>
2025-06-06 18:00:01 +0200

    Add files via upload
    Added support for 7-segment display and 4-digit 7-segment display

--- p8o_pi.py
+++ p8o_pi.py
@@ -21,12 +21,119 @@
 # Author: peppe8o
 # Blog: https://peppe8o.com
 #
-#
+# v2
+# date: 5th Jun, 2025
 
 import gpiozero
 from time import sleep
+from gpiozero import DigitalOutputDevice
+import re
+import multiprocessing
 
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# Class for 7-segment display
+# --------------------------------------------------------------------------------------------------------------------------------------------
+class seven_segment:
+  def __init__(self, a_PIN, b_PIN, c_PIN, d_PIN, e_PIN, f_PIN, g_PIN, dot_PIN):
+    self.a = gpiozero.DigitalOutputDevice(14)
+    self.b = gpiozero.DigitalOutputDevice(15)
+    self.c = gpiozero.DigitalOutputDevice(18)
+    self.d = gpiozero.DigitalOutputDevice(23)
+    self.e = gpiozero.DigitalOutputDevice(24)
+    self.f = gpiozero.DigitalOutputDevice(25)
+    self.g = gpiozero.DigitalOutputDevice(8)
+    self.dot = gpiozero.DigitalOutputDevice(7)
+
+    self.arrSeg = {\
+          "0":[1,1,1,1,1,1,0],\
+          "1":[0,1,1,0,0,0,0],\
+          "2":[1,1,0,1,1,0,1],\
+          "3":[1,1,1,1,0,0,1],\
+          "4":[0,1,1,0,0,1,1],\
+          "5":[1,0,1,1,0,1,1],\
+          "6":[1,0,1,1,1,1,1],\
+          "7":[1,1,1,0,0,0,0],\
+          "8":[1,1,1,1,1,1,1],\
+          "9":[1,1,1,1,0,1,1]}
+    self.display_list = [self.a,self. b, self.c, self.d, self.e, self.f, self.g]
+
+  def show(self, to_display, dotValue=False):
+    value_to_display = str(to_display)
+
+    if value_to_display not in self.arrSeg:
+      raise Exception("Value not available in the seven_segment.show() dictionary")
+
+    for i in range(0,len(self.display_list)):
+      self.display_list[i].value = self.arrSeg[value_to_display][i]
+
+    if dotValue:
+      self.dot.value = 1
+    else:
+      self.dot.value = 0
+
+  def clear(self):
+    for i in range(0,len(self.display_list)):
+      self.display_list[i].value = 0
+    self.dot.value = 0
+
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# Class for 4-digit 7-segment display
+# --------------------------------------------------------------------------------------------------------------------------------------------
+
+class four_digit_seven_segment:
+  def __init__(self, a_PIN, b_PIN, c_PIN, d_PIN, e_PIN, f_PIN, g_PIN, dot_PIN, sel1_PIN, sel2_PIN, sel3_PIN, sel4_PIN):
+
+    self.selectors = [gpiozero.DigitalOutputDevice(sel1_PIN),\
+                      gpiozero.DigitalOutputDevice(sel2_PIN),\
+                      gpiozero.DigitalOutputDevice(sel3_PIN),\
+                      gpiozero.DigitalOutputDevice(sel4_PIN)]
+
+    self.digit_display = seven_segment(a_PIN, b_PIN, c_PIN, d_PIN, e_PIN, f_PIN, g_PIN, dot_PIN)
+    self.proc = multiprocessing.Process(target=self.show_4_control, args=[""])
+
+  def show_4_control(self, to_show):
+   format_matches = re.search("^\d\.?\d\.?\d\.?\d\.?", to_show) is not None
+
+   if not format_matches:
+      raise Exception("Value not matching the required format. It must be a string of 4 numbers, with optional dot (.) after each number")
+
+   while True:
+    to_display = []
+    i=0
+    while i < len(to_show):
+      if i+1 < len(to_show) and to_show[i+1] == ".":
+        to_display.append(to_show[i:i+2])
+        i=i+2
+      else:
+        to_display.append(to_show[i])
+        i=i+1
+
+    for i in range(0,4):
+      if len(to_display[i])== 2:
+        dotValue = True
+      else:
+        dotValue = False
+      for s in range(0,4): self.selectors[s].on()
+      self.selectors[i].off()
+      self.digit_display.show(to_display[i][0], dotValue)
+      sleep(0.003)
+
+  def show_4(self, to_show):
+    if self.proc.is_alive(): self.proc.terminate()
+    self.proc = multiprocessing.Process(target=self.show_4_control, args=[to_show])
+    self.proc.start()
+
+  def clear(self):
+    self.proc.terminate()
+
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
 # Class for traffic light module, using the GPIOZERO LEDBoard
+# --------------------------------------------------------------------------------------------------------------------------------------------
 class traffic_light:
   def __init__(self, red_LED, yellow_LED, green_LED):
     # NOTE: not well documented, however the LEDBoard seems to resort the LEDs order when assigned with a label.
